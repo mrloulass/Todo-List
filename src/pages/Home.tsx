402c313f870c9104d7e4b05486d5246f214e70bf
Louis Lassegue <louislassegue@gmail.com>
2021-10-19 15:47:48 -0700

    Added the feature to add task to list.

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type Props = {};
+
+const Home: React.FC<Props> = () => {
+  return (
+  <h2>Home </h2>
+  )
+};
+
+export default Home;
---
+++ src/pages/Tasks.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type Props = {};
+
+const Tasks: React.FC<Props> = () => {
+  return (
+    <h2>Tasks</h2>
+  )
+}
+
+export default Tasks;
