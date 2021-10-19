8502ff69f5fd7a12a2805f63d35bcc5d1119f8aa
Louis Lassegue <louislassegue@gmail.com>
2021-10-18 17:17:19 -0700

    Setup component layout for app

---
+++ src/pages/List.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const List: React.FC<Props> = () => {
+  return (
+    <div>List Page</div>
+  )
+}
+
+export default List;
---
+++ src/pages/Task.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Task: React.FC<Props> = () => {
+  return (
+    <div>Task Page</div>
+  )
+}
+
+export default Task;
