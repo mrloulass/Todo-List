c5c3f91c4d40b34b3724f8a8a4b64e93e0ebe513
Louis Lassegue <louislassegue@gmail.com>
2021-11-03 17:04:41 -0700

    Added style for NavBar

--- src/nav/NavBar.tsx
+++ src/nav/NavBar.tsx
@@ -1,41 +1,7 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-import styled from 'styled-components';
-import {colors} from '../styles/colors'
+import {Nav, NavButton} from '../styles/NavBarStyle'
+
 type Props = {};
-
-const Nav = styled.nav`
-  display: flex;
-`;
-
-const NavButton = styled(NavLink)`
-  background: ${colors.white};
-  color: ${colors.black};
-  height: 62px;
-  width: 120px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-decoration: none;
-  
-  &.homeButton {
-    background: ${colors.blue};
-    color: ${colors.white};
-  }
-  &.listButton {
-    background: ${colors.mint};
-    color: ${colors.white};
-  }
-  &.taskButton {
-    background: ${colors.pink};
-    color: ${colors.white};
-  }
-
-  &.active {
-    background: ${colors.yellow};
-    color: ${colors.black};
-  }
-`;
 
 const NavBar: React.FC<Props> = () => {
   return (
